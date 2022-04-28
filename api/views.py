3e419984c921086802727fe73c5a1c2edcca96ba
Kidus Guade <kidusguade41@gmail.com>
2022-04-28 00:32:10 -0700

    response many list and single list view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 # Create your views here.
 
+@api_view(['GET'])
 def get_routes(request):
     
-    return JsonResponse('our api', safe=False)
+    return Response('our api')
+
+@api_view(['GET'])
+
+def get_notes(request):
+    notes = Note.objects.all()
+    serialzer = NoteSerializer(notes, many=True)
+    return Response(serialzer.data)
+
+@api_view(['GET'])
+def get_note(request, pk):
+    notes = Note.objects.get(id=pk)
+    serialzer = NoteSerializer(notes, many=False)
+    return Response(serialzer.data)
 
