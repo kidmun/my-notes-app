1b4a490a392ceb5ee24975fb6db0b3414c67634b
Kidus Guade <kidusguade41@gmail.com>
2022-04-27 23:35:56 -0700

    create the note model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+from re import T
+from turtle import update
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
